07dd0fb12cb8014ea84a90ac59ab0952b46f3f79
ElliotWhiley <elliot.whiley94@gmail.com>
2021-04-17 16:13:45 +1200

    Destructure data straight from axios response

--- src/services/GitHubService.js
+++ src/services/GitHubService.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const getGitHubProfile = async (userName) => {
 	try {
-		const response = await axios.get(
+		const { data } = await axios.get(
 			`https://api.github.com/users/${userName}`
 		);
-		return response.data;
+		return data;
 	} catch (exception) {
 		console.log("Error retrieving GitHub profile", exception);
 		throw exception;
--- src/services/JokeService.js
+++ src/services/JokeService.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const getRandomJoke = async () => {
 	try {
-		const response = await axios.get(
+		const { data } = await axios.get(
 			"https://official-joke-api.appspot.com/jokes/random"
 		);
-		return response.data;
+		return data;
 	} catch (exception) {
 		console.log("Error retrieving GitHub profile", exception);
 		throw exception;
