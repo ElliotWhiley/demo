1c538bd4bff2a029cf9fdd3fe16113737740903a
ElliotWhiley <elliot.whiley94@gmail.com>
2021-04-18 09:34:27 +1200

    Implement fetching pokemon generation data from open pokemon graphql api

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,8 +1,44 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { useState, useEffect, Fragment } from "react";
+
+const pokemonClient = new ApolloClient({
+	uri: "https://beta.pokeapi.co/graphql/v1beta",
+	cache: new InMemoryCache(),
+});
+
 const Pokemon = () => {
+	const [generations, setGenerations] = useState([]);
+
+	useEffect(async () => {
+		var { data } = await pokemonClient.query({
+			query: gql`
+				query pokemonSpecies {
+					generations: pokemon_v2_generation {
+						pokemon_species: pokemon_v2_pokemonspecies_aggregate {
+							aggregate {
+								count
+							}
+						}
+						name
+					}
+				}
+			`,
+		});
+		setGenerations(data.generations);
+	}, []);
+
 	return (
-		<div>
-			<h2>Pokemon</h2>
-		</div>
+		<Fragment>
+			{generations.length ? (
+				<div>
+					{generations.map((generation) => (
+						<div>{generation.name}</div>
+					))}
+				</div>
+			) : (
+				<span>loading...</span>
+			)}
+		</Fragment>
 	);
 };
 
