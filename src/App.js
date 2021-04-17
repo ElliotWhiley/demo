eb22567aefc8f2d35b64b5408cb8d93e1aa5b9e0
ElliotWhiley <elliot.whiley94@gmail.com>
2021-04-17 14:25:02 +1200

    Implement random joke from official joke api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (props) => {
+	const [joke, setJoke] = useState({});
+
+	const getRandomJoke = async () => {
+		const response = await axios.get(
+			"https://official-joke-api.appspot.com/jokes/random"
+		);
+		setJoke(response.data);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<button onClick={getRandomJoke}>Joke</button>
+				<div>{joke.setup}</div>
+				<div>{joke.punchline}</div>
+			</header>
+		</div>
+	);
+};
 
 export default App;
