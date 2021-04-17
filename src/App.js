652f7a7ca94dcdffe3f4487b933366d43074f0bb
ElliotWhiley <elliot.whiley94@gmail.com>
2021-04-17 14:27:57 +1200

    Hide joke punchline by default, only displaying if the user clicks punchline

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import { useState } from "react";
 
 const App = (props) => {
 	const [joke, setJoke] = useState({});
+	const [displayPunchline, setDisplayPunchline] = useState(false);
 
 	const getRandomJoke = async () => {
 		const response = await axios.get(
 			"https://official-joke-api.appspot.com/jokes/random"
 		);
 		setJoke(response.data);
+		setDisplayPunchline(false);
 	};
 
 	return (
 		<div className="App">
 			<header className="App-header">
 				<button onClick={getRandomJoke}>Joke</button>
+				<button onClick={() => setDisplayPunchline(true)}>
+					Punchline
+				</button>
 				<div>{joke.setup}</div>
-				<div>{joke.punchline}</div>
+				{displayPunchline && <div>{joke.punchline}</div>}
 			</header>
 		</div>
 	);
